b9bb37295e0cc5b008d88948f17f79e21319466c
Martin Winther <martin.winther@protonmail.com>
2023-11-27 10:39:30 +0100

    3.1

--- index.js
+++ index.js
@@ -25,3 +25,27 @@ let persons = [
 		number: "39-23-6423122",
 	},
 ];
+
+app.get("/", (request, response) => {
+	response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+	res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find((person) => person.id === id);
+
+	if (person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
