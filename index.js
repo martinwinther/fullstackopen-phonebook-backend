005d0a8ea5bea96392db2cde50b2cb5fba846267
Martin Winther <martin.winther@protonmail.com>
2023-11-28 11:48:19 +0100

    3.4

--- index.js
+++ index.js
@@ -51,6 +51,17 @@ app.get("/api/persons/:id", (req, res) => {
 	}
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	persons = persons.filter((person) => person.id !== id);
+
+	if (person) {
+		res.status(204).end();
+	} else {
+		res.status(404).end();
+	}
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
