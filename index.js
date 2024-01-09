464cf046ae806e9ac8f6ae8cdf291d7c1abb0e77
Martin Winther <martin.winther@protonmail.com>
2024-01-09 10:04:32 +0100

    3.19-3.21

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === "CastError") {
 		return response.status(400).send({ error: "malformatted id" });
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message });
 	}
 
 	next(error);
@@ -107,7 +109,7 @@ app.post("/api/persons", (req, res, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-	const body = request.body;
+	const { name, number } = request.body;
 
 	const person = {
 		id: generateId(),
@@ -115,7 +117,11 @@ app.put("/api/persons/:id", (request, response, next) => {
 		number: body.number,
 	};
 
-	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+	Person.findByIdAndUpdate(
+		request.params.id,
+		{ name, number },
+		{ new: true, runValdiators: true, context: "query" }
+	)
 		.then((updatedPersone) => {
 			response.json(updatedPerson);
 		})
--- models/person.js
+++ models/person.js
@@ -15,8 +15,21 @@ mongoose
 	});
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+	name: {
+		type: String,
+		minLength: 3,
+		required: true,
+	},
+	number: {
+		type: String,
+		minLength: 8,
+		validate: {
+			validator: function (v) {
+				return /^\d{2,3}-\d+$/.test(v);
+			},
+		},
+		message: (props) => `${props.value} is not a valid phone number!`,
+	},
 });
 
 personSchema.set("toJSON", {
